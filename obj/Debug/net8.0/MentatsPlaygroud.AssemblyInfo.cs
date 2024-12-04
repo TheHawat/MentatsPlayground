920d9e876b09e485e1bf03ed852feb36b8691521
Thufir Hawat <hawat@Bartoszs-MacBook-Pro.local>
2024-12-04 21:35:31 +0100

    added changes

--- obj/Debug/net8.0/MentatsPlaygroud.AssemblyInfo.cs
+++ obj/Debug/net8.0/MentatsPlaygroud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MentatsPlaygroud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f00cfb16bac6988399acd87a3a7eb494b7f29039")]
 [assembly: System.Reflection.AssemblyProductAttribute("MentatsPlaygroud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MentatsPlaygroud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
